891024b035ab510d93320982dadf47f5f806b1dc
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2018-03-12 22:50:55 +0000

    working version

--- server/tutorialThree.js
+++ server/tutorialThree.js
@@ -1,6 +1,33 @@
 /**
  * This is an example with express and graphQl
+ * 
+ * https://scotch.io/@codediger/build-a-simple-graphql-api-server-with-express-and-nodejs
  */
-const experss     = require('express');
-const buildSchema = require('graphql');
-const graphqlHTTP = require('express-graphql');
+const express     = require('express'),
+app               = express(),
+{buildSchema}     = require('graphql'),
+graphqlHTTP       = require('express-graphql');
+
+let port = 3000;
+
+let schema = buildSchema('type Query { marvelCharacter : String, powers : String}')
+
+let root = {
+    marvelCharacter : ()=> {
+        return 'Iron Man'
+    },
+
+    powers : () => {
+        return 'genius playboy billionaire philanthropist '
+    }
+}
+
+app.use(('/', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true      
+})));
+
+app.listen(port);
+
+console.log('GraphQL API server running at localhost:'+ port);
